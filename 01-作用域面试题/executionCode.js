0253c6782e87a82ef773b3d892d3fc1f38bb46e4
xionglp <785874752@qq.com>
2022-03-31 06:39:24 +0800

    js代码的执行过程

---
+++ 01-作用域面试题/executionCode.js
@@ -0,0 +1,7 @@
+var name = "xionglp";
+
+var num1 = 1;
+var num2 = 2;
+
+var result = num1 + num2;
+console.log(result)
---
+++ 01-作用域面试题/main.js
@@ -0,0 +1,14 @@
+
+
+/*
+  1、浏览器的渲染过程？
+  2、认识浏览器内核（google在使用的是Blik）
+  3、JS引擎： google浏览器使用的V8引擎
+
+  V8引擎的原理：
+  1、javascript源代码通过Parser模块转换成抽象语法树；
+  2、通过Ignition解释器将AST抽象语法树转换成ByteCode字节码；
+  3、通过TurboFan编译器将字节码编译成CPU可执行的机器码；
+
+
+*/

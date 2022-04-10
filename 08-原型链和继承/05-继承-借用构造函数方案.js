f3f7cd4968f1d04c7df40ad4e7004b3272492bab
xionglp <785874752@qq.com>
2022-04-10 21:33:20 +0800

    借用构造函数方案实现继承

---
+++ 08-原型链和继承/05-继承-借用构造函数方案.js
@@ -0,0 +1,30 @@
+// 父类： 公共方法和属性
+function Person(name, friends) {
+  this.name = name
+  this.friends = friends
+}
+
+Person.prototype.eating = function() {
+  console.log(this.name + "在吃东西")
+}
+
+// 子类： 特有的方法和属性
+function Student(name, friends, sno) {
+  Person.call(this, name, friends);
+  this.sno = sno
+}
+
+Student.prototype = new Person()
+Student.prototype.studying = function() {
+  console.log(this.name + "在跑步")
+}
+
+var stu = new Student("kobe", ["james"], 112);
+console.log(stu);
+// stu.studying()
+// stu.eating()
+
+var stu1 = new Student("kobe", ["james"], 112)
+var stu2 = new Student("why", ["lilei"], 113)
+stu1.friends.push("lucy")
+console.log(stu2.friends)

8fa3f72aa513b61754d2d5a30cbf65d858dbf10a
xionglp <785874752@qq.com>
2022-04-10 11:31:57 +0800

    函数的原型理解、利用构造函数和原型创建对象

--- 07-JS创建对象方案/04-对象的原型的理解.js
+++ 07-JS创建对象方案/04-对象的原型的理解.js
@@ -1,5 +1,3 @@
-
-
 var obj = {
   name: "xionglp"
 }
@@ -7,15 +5,12 @@ var obj = {
 // 每个对象中都有一个[[prototype]], 这个属性称为对象的原型（隐式原型）
 // 浏览器提供了一个__proto__ 属性，可以让我们查看这个对象的原型
 // ES5之后提供了一个Object.getPrototypeOf 来查看对象的原型
-
 console.log(obj.__proto__)
 console.log(Object.getPrototypeOf(obj))
-
 
 // 原型的作用：
 // 当我们从一个对象中获取某一个属性时，它会触发[[get]]操作
 // 1. 如果在当前对象中查找到对应的属性，直接使用；
 // 2. 如果没有找到，那么它会沿着它的原型链去查找[[prototype]]
-
 obj.__proto__.age = 18;
 console.log(obj.age);

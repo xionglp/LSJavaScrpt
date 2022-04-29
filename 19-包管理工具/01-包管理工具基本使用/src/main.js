8064af89121901736263a2203411b7b36b91cf4c
xionglp <785874752@qq.com>
2022-04-29 21:36:35 +0800

    npm yarn 包管理工具的使用

---
+++ 19-包管理工具/01-包管理工具基本使用/src/main.js
@@ -0,0 +1,8 @@
+import axios from "axios"
+import Vue from "vue"
+
+axios.get("url").then(res => {
+
+})
+
+const app = new Vue({})
---
+++ 19-包管理工具/05-使用自己发布的包/src/index.js
@@ -0,0 +1,7 @@
+// import { add, sub } from "ls_utils_common"
+
+const { add, sub, mul } = require("ls_utils_common") // commonJS导入
+
+console.log(add(10, 20))
+console.log(sub(30, 15))
+console.log(mul(10, 20))

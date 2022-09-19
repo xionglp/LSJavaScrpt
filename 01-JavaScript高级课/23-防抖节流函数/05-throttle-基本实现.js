90603cf48c60f2af653807c5c05430c992d3affc
xionglp <785874752@qq.com>
2022-09-20 06:53:01 +0800

    优化

--- 01-JavaScript高级课/23-防抖节流函数/05-throttle-基本实现.js
+++ 01-JavaScript高级课/23-防抖节流函数/05-throttle-基本实现.js
@@ -7,11 +7,11 @@ function throttle(fn, interval) {
   // 记录上一次的开始时间
   let lastTime = 0
 
-  const _throttle = function() {
+  const _throttle = function () {
     // 获取当前事件触发时的时间
     const nowTime = new Date().getTime()
 
-    // 使用当前触发的时间和之前的时间间隔以及上一次开始的时间，计算出还剩余多长事件需要去触发函数
+    // 使用当前触发的时间和之前的时间间隔以及上一次开始的时间，计算出还剩余多长时间需要去触发函数
     const remainTime = interval - (nowTime - lastTime)
     if (remainTime <= 0) {
       fn()
